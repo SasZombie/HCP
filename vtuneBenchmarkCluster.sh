0afbe4ff02a4000754d9cfb827f29b3d60c2d6f8
saszombie <gigelusas81@gmail.com>
2026-04-16 14:08:15 +0300

    C

--- vtuneBenchmarkCluster.sh
+++ vtuneBenchmarkCluster.sh
@@ -29,7 +29,6 @@ do
 
     apptainer exec --bind /tmp:/tmp ../imagineHPC.sif \
         vtune -collect hotspots \
-        -knob sampling-mode=sw \
         -result-dir $RESULTS_DIR \
         -start-paused \
         ./venv/bin/python3 modular_main.py $TEST_TYPE $T
