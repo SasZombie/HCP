bbd27c33a5f7d1ae214f11b40fede763f48a287a
saszombie <gigelusas81@gmail.com>
2026-04-24 15:49:50 +0300

    Cpp compilation script

---
+++ compileCpp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --partition=haswell
+#SBATCH --job-name=compilation
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8       
+#SBATCH --mem=8G                
+#SBATCH --output=compile_%j.out   
+#SBATCH --time=00:15:00   
+
+NPROCS=${SLURM_CPUS_PER_TASK:-1}
+
+echo "Starting compilation using $NPROCS cores..."
+
+apptainer exec \
+    --bind .:/app \
+    --pwd /app \
+    --cleanenv \
+    ../imagineHPC.sif \
+    /bin/bash -c "cd CppBindings && \
+                  cmake --preset release && \
+                  cmake --build --preset release -j $NPROCS"
+
+echo "Compilation finished with exit code $?"
